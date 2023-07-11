942620cf5f14b5154f88f8b7cd5645f7c50e2fa0
Vansh Singh <vansh2@ualberta.ca>
2023-07-11 10:16:05 +0530

    Created Product Lisitng

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import products from "./app/data/products"
+import { FlatList } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <FlatList
+  data={products}
+  renderItem={({ item }) => (
+    <View style={{ width: "50%", padding: 1 }}>
+      <Image source={{ uri: item.image }} style={styles.image} />
+      <Text style={{alignSelf: "center",fontWeight: "bold"}}>{item.name}</Text>
     </View>
+  )}
+  numColumns={2}
+/>
   );
 }
 
@@ -16,5 +24,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image:{
+    aspectRatio: 1,
   },
 });

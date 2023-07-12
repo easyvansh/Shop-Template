411105430b62aa083ad92c741a56525e23a0d34a
Vansh Singh <vansh2@ualberta.ca>
2023-07-12 15:53:46 +0530

    Added Product Count on Cart

--- Navigator.js
+++ Navigator.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomePage from './app/screens/HomePage';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomePage from "./app/screens/HomePage";
+import ProductDetails from "./app/screens/ProductDetails";
+import ShoppingCart from "./app/screens/ShoppingCart";
+import { Pressable, Text } from "react-native";
+import {FontAwesome5} from '@expo/vector-icons'
+import { useSelector } from 'react-redux';
+import { selectNumberOfItems } from './app/store/cartSlice';
+
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
+  const numberOfItems = useSelector(selectNumberOfItems)
   return (
     <NavigationContainer>
-        <Stack.Navigator >
-            <Stack.Screen name = "Home" component = {HomePage} />
-        </Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{ contentStyle: { backgroundColor: "white" } }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomePage}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <Pressable
+                onPress={() => navigation.navigate("Cart")}
+                style={{ flexDirection: "row", padding: 20 }}
+              >
+                <FontAwesome5 name="shopping-cart" size={18} color="gray" />
+                <Text style={{ marginLeft: 5, fontWeight: "500" }}>
+                  {numberOfItems}
+                </Text>
+              </Pressable>
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="Product Details"
+          component={ProductDetails}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <Pressable
+                onPress={() => navigation.navigate("Cart")}
+                style={{ flexDirection: "row", padding: 20 }}
+              >
+                <FontAwesome5 name="shopping-cart" size={18} color="gray" />
+                <Text style={{ marginLeft: 5, fontWeight: "500" }}>
+                  {numberOfItems}
+                </Text>
+              </Pressable>
+            ),
+          })}
+        />
+        <Stack.Screen name="Cart" component={ShoppingCart} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };

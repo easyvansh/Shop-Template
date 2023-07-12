3fa0d53da37b7691b5d85efbdeb2c869add7394e
Vansh Singh <vansh2@ualberta.ca>
2023-07-12 15:50:09 +0530

    Upated Formating

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View ,Image} from 'react-native';
-import products from "./app/data/products"
-import { FlatList } from 'react-native';
-import AppNavigator from './Navigator';
+import { store } from "./app/store";
+import AppNavigator from "./Navigator";
+import { Provider } from "react-redux";
 
 export default function App() {
   return (
-    <AppNavigator/>
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
   );
 }
-

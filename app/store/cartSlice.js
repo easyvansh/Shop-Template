93982ef4de088aa31cc97ff8b45022ed28efe9b7
Vansh Singh <vansh2@ualberta.ca>
2023-07-12 15:56:57 +0530

    Implemented State Management with Redux Toolkit

--- app/store/cartSlice.js
+++ app/store/cartSlice.js
@@ -1,5 +1,6 @@
 
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createSelector } from '@reduxjs/toolkit';
+
 
 const initialState = {
   items: [],
@@ -12,11 +13,44 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     addCartItem: (state, action) => {
-        const newProduct = action.payload.product;
-      
+      const newProduct = action.payload.product;
+      const cartItem = state.items.find((item) => item.product.id === newProduct.id)
+      if (cartItem){
+        cartItem.quantity +=1;
+    }else{
         state.items.push({ product: newProduct, quantity: 1 });
-      },
-    changeQuantity: (state, action) => {
-    },
+    }
   },
-});
+  changeQuantity: (state, action) => {
+    const {productId,amount } = action.payload;
+    const cartItem = state.items.find((item) => item.product.id === productId)
+    if(cartItem){
+        cartItem.quantity+=amount;
+    }if(cartItem.quantity<=0){
+        state.items = state.items.filter((item) => item !== cartItem)            
+    }
+},
+},
+})
+
+
+export const selectNumberOfItems = (state) => state.cart.items.length;
+export const selectSubtotal = (state) =>
+  state.cart.items.reduce(
+    (sum, item) => sum + item.product.price * item.quantity,
+    0
+  );
+
+  export const selectSelf = (state) => state.cart;
+  export const selectDeliveryPrice = createSelector(
+    selectSelf,
+    selectSubtotal,
+    (state, subtotal) =>
+      subtotal > state.freeDeliveryFrom ? 0 : state.deliveryPrice
+  );
+  
+  export const selectTotal = createSelector(
+    selectSubtotal,
+    selectDeliveryPrice,
+    (subtotal, delivery) => subtotal + delivery
+  );
--- app/store/index.js
+++ app/store/index.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { productsSlice } from './productsSlice';
+import { cartSlice } from './cartSlice';
+
 
 export const store = configureStore({
   reducer: {
     products: productsSlice.reducer,
+    cart: cartSlice.reducer,
   },
 });

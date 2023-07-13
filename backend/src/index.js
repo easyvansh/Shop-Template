951d9b84b025303c243a0929329b82fe66a04354
Vansh Singh <vansh2@ualberta.ca>
2023-07-13 12:20:19 +0530

    Created Backend Using Node.js And Express.js

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const productRoutes = require('./router/productRoutes');
+
+const app = express();
+const PORT = 3000;
+
+app.use('/products',productRoutes);
+
+app.get('/', (req, res) => {
+	res.send("<h2>Hello world!</h2>");
+});
+
+app.listen(PORT, () => {
+  console.log('API is listening on port ', PORT);
+});
